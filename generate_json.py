bb8062742a6e65020bfeff73bfd611b07aa38877
Hans Adema <hans.adema@wearespindle.com>
2017-10-27 17:11:01 +0200

    Remove testing files

--- generate_json.py
+++
@@ -1,8 +0,0 @@
-from cacofonisk import AmiRunner, JsonReporter
-from cacofonisk.channel import DebugChannelManager
-
-reporter = JsonReporter('tests/fixtures/xfer_blind/xfer_blind_reject.json')
-runner = AmiRunner([
-    {'host': '172.20.0.12', 'username': 'cacofonisk', 'password': 'bard', 'port': 5038},
-], reporter, channel_manager=DebugChannelManager)
-runner.run()
